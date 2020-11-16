9461c4b85f193e54e09ac453cdecaa6baeeded5d
Afra Rian <afraryan44@gmail.com>
2020-11-16 16:23:23 +0700

    Model added

---
+++ ExamOnline/Models/Question.cs
@@ -0,0 +1,25 @@
+﻿using ExamOnline.Bases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamOnline.Models
+{
+    [Table("TB_M_Question")]
+    public class Question : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Quest { get; set; }
+        public string AnswerA { get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+        public string AnswerD { get; set; }
+        public string CorrectAnswer { get; set; }
+        public int SegmentId { get; set; }
+        public Segment Segment { get; set; }
+    }
+}
