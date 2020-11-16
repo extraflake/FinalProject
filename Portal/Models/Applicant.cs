e0bcb7a20d617c25890ca4975974090630136c6f
dionisiusyose <dionisiusyose11@gmail.com>
2020-11-16 19:03:14 +0700

    migrate table

--- Portal/Models/Applicant.cs
+++ Portal/Models/Applicant.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Portal.Bases;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 namespace Portal.Models
 {
     [Table("TB_M_Applicant")]
-    public class Applicant
+    public class Applicant : IEntity
     {
         [Key]
         public int Id { get; set; }
