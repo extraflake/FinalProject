df79f569e1a0f8421bc9b6e68170fb8f651e6ab3
haidaraldi <haidaraw34@gmail.com>
2020-11-16 17:00:48 +0700

    Add Migration to Connect Server

---
+++ UserManagement/Models/UserRole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagement.Microservices.Models
+{
+    [Table("TB_T_UserRole")]
+    public class UserRole
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
