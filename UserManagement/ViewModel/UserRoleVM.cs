58b2cdbc0adf13a654fda2927ea02360d0172355
haidaraldi <haidaraw34@gmail.com>
2020-11-17 11:12:14 +0700

    Add API Register, Login, ChangePassword, Forgot Password

---
+++ UserManagement/ViewModel/UserRoleVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagement.ViewModel
+{
+    public class UserRoleVM
+    {
+        public int Id { get; set; }
+
+        public int User_UserId { get; set; }
+        public string User_Email { get; set; }
+        public string User_Password { get; set; }
+
+        public int Role_RoleId { get; set; }
+        public string Role_Name { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Phone { get; set; }
+    }
+}
