e8d4e76ead93ba343385ddea6b844f6201d5d377
Kevin Hendra William <kevinhendrawilim@gmail.com>
2020-11-18 15:50:15 +0700

    Update ExamDetailsController.cs

--- ExamOnline/Controllers/ExamDetailsController.cs
+++ ExamOnline/Controllers/ExamDetailsController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using ExamOnline.Dapper_ORM;
+using ExamOnline.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -22,6 +25,16 @@ namespace ExamOnline.Controllers
             dapper = _dapper;
         }
 
+        [HttpPost]
+        public Task<List<ExamDetailVM>> Create(ExamDetailVM examDetailVM)
+        {
+            var dbparams = new DynamicParameters();
+            dbparams.Add("@DurationId", examDetailVM.DurationId, DbType.Int32);
+            dbparams.Add("@GradeId", examDetailVM.GradeId, DbType.Int32);
+            dbparams.Add("@FinalScore", examDetailVM.FinalScore, DbType.Int32);
+            var result = Task.FromResult(dapper.GetAll<ExamDetailVM>("[SP_Create_ExamDetail]", dbparams, commandType: CommandType.StoredProcedure));
+            return result;
+        }
 
 
     }
