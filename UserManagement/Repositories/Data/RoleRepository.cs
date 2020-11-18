555a3f8153cab02e49d3dc2257309d19ebc2e41a
agung ali akbar <agungaliakbar8@gmail.com>
2020-11-18 10:36:33 +0700

    AddRole

---
+++ UserManagement/Repositories/Data/RoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.Microservices.Context;
+using UserManagement.Microservices.Models;
+
+namespace UserManagement.Repositories.Data
+{
+    public class RoleRepository : GeneralRepository<Role, MyContext>
+    {
+        public RoleRepository(MyContext myContext): base(myContext)
+        {
+
+        }
+    }
+}
