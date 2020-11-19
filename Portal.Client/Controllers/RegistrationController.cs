71af482e032d3012d9168d22a8de86dcdf88296e
Ridho Novradinata <ridhonovradinata@gmail.com>
2020-11-19 17:05:22 +0700

    get file to dropdown

--- Portal.Client/Controllers/RegistrationController.cs
+++ Portal.Client/Controllers/RegistrationController.cs
@@ -1,13 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Portal.Client.Controllers
 {
     public class RegistrationController : Controller
     {
+        [HttpGet]
+        public ActionResult GetNamePosition(Portal.Models.Position position)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44307");
+                MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                string data = JsonConvert.SerializeObject(position);
+                var contentData = new StringContent(data, Encoding.UTF8, "application/json");
+                var response = client.GetAsync("/api/positions").Result;
+                //ViewBag.Message = response.Content.ReadAsStringAsync().Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return Json(response.Content.ReadAsStringAsync().Result.ToString());
+                }
+                else
+                {
+                    return Content("GAGAL");
+                }
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetNameReference(Portal.Models.Reference reference)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44307");
+                MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                string data = JsonConvert.SerializeObject(reference);
+                var contentData = new StringContent(data, Encoding.UTF8, "application/json");
+                var response = client.GetAsync("/api/references").Result;
+                //ViewBag.Message = response.Content.ReadAsStringAsync().Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return Json(response.Content.ReadAsStringAsync().Result.ToString());
+                }
+                else
+                {
+                    return Content("GAGAL");
+                }
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetNameSkill(Portal.Models.Skill skill)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44307");
+                MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                string data = JsonConvert.SerializeObject(skill);
+                var contentData = new StringContent(data, Encoding.UTF8, "application/json");
+                var response = client.GetAsync("/api/skills").Result;
+                //ViewBag.Message = response.Content.ReadAsStringAsync().Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return Json(response.Content.ReadAsStringAsync().Result.ToString());
+                }
+                else
+                {
+                    return Content("GAGAL");
+                }
+            }
+        }
         public IActionResult Index()
         {
             return View();
