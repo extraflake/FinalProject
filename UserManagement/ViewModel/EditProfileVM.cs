0de758f848d0a2e0c6081edcf2bbf0155689cd16
haidaraldi <haidaraw34@gmail.com>
2020-11-19 14:20:29 +0700

    Add EditProfile Before Applicant Bootcamp

--- UserManagement/ViewModel/EditProfileVM.cs
+++ UserManagement/ViewModel/EditProfileVM.cs
@@ -8,22 +8,23 @@ namespace UserManagement.ViewModel
     public class EditProfileVM
     {
         public int Id { get; set; }
-        public string FullName { get; set; }
-        public string User_Email { get; set; }        
-        public string Phone { get; set; }
+        public string FirstName{ get; set; }
+        public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
         public string Gender { get; set; }
-        public int Religion_Id { get; set; }
-        public int Religion_Name { get; set; }  
+        public int ReligionId { get; set; }
+        public int EmployeeId { get; set; }        
 
-        public string Univ_Id { get; set; }
-        public string Univ_Name { get; set; }
-        public string Dept_Id { get; set; }
-        public string Dept_Name { get; set; }
+        public string User_Email { get; set; }
+        public string Phone { get; set; }
+        public int UserId { get; set; }
+        
+        public string UniversityId { get; set; }        
+        public string DepartmentId { get; set; }
 
-        public int UnivDept_Id { get; set; }
-        public string Level { get; set; }
         public string GPA { get; set; }
-        public string GraduateYear { get; set; }
+        public string Degree { get; set; }
+        public string GraduateYear { get; set; }         
+                
     }
 }
