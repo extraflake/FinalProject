3f74b1c11936b0e8016ce631a1a74bc42a9464f8
Afra Rian <afraryan44@gmail.com>
2020-11-20 15:58:51 +0700

    isActive Schedule

---
+++ ExamOnline/Migrations/20201120085354_updateSchedule.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExamOnline.Migrations
+{
+    public partial class updateSchedule : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "TB_M_Schedule",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ApplicantId",
+                table: "TB_M_Duration",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "TB_M_Schedule");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicantId",
+                table: "TB_M_Duration",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
