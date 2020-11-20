44a764b9f904d59eb0abfbc9fde75ae639cad3e1
dionisiusyose <dionisiusyose11@gmail.com>
2020-11-20 19:04:40 +0700

    add send mail

--- Portal/Controllers/ApplicantsController.cs
+++ Portal/Controllers/ApplicantsController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
 using System.Threading.Tasks;
 using Dapper;
 using Microsoft.AspNetCore.Http;
@@ -76,12 +80,53 @@ namespace Portal.Controllers
             await myContext.Applicants.AddAsync(data);
             var result = await myContext.SaveChangesAsync();
 
-            var getFile = Download(Doc.Id);
+            //var getFile = Download(Doc.Id);
+            MemoryStream memorystream = new MemoryStream(data.File.DataFile);
+
+            //System.Net.Mail.Attachment attachment;
+            //attachment = new System.Net.Mail.Attachment(getFile.FileDownloadName);
+
+            string myEmail = ""; // Email dimasukkan terlebih dahulu
+            string body = "Applicant data \n" +
+                $"Posisi    : {data.Position.Name} \n" +
+                $"Referensi : {data.Reference.Name} \n" +
+                $"Skill     : {data.Skills.ToList()} \n" +
+                $"File      : {data.File.Name}";
+
+            var msgbody = new System.Text.StringBuilder();
+            msgbody.AppendLine("<html><body>");
+            msgbody.AppendLine("<h4>Tes Applicant Data</h4>");
+            msgbody.AppendLine($"<p>Posisi      : {data.Position.Name}</p>");
+            msgbody.AppendLine($"<p>Referensi   : {data.Reference.Name}</p>");
+            msgbody.AppendLine($"<p>Nama file   : {data.File.Name}</p>");
+            msgbody.AppendLine("</body></html>");
+
+            string htmlBody = msgbody.ToString();
+
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(myEmail, ""); // Password harus dimasukkan terlebih dahulua
+            //MailMessage mm = new MailMessage("donotreply@gmail.com", myEmail, "This the data of applicant", body);
+            MailMessage mm = new MailMessage();
+            mm.From = new MailAddress("donotreply@gmail.com");
+            mm.To.Add(myEmail);
+            mm.Subject = "Applicant Data";
+            mm.IsBodyHtml = true;
+            mm.Body = htmlBody;
+
+            mm.Attachments.Add(new Attachment(memorystream, data.File.Name, mediaType: MediaTypeNames.Application.Pdf));
+
+            mm.BodyEncoding = UTF8Encoding.UTF8;
+            mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+            client.Send(mm);
 
             return Ok(result);
         }
-
-
 
         public FileResult Download(int id)
         {
@@ -89,6 +134,22 @@ namespace Portal.Controllers
             string fileName = file.Name;
             byte[] pdfasBytes = file.DataFile;
             return File(pdfasBytes, "application/pdf", fileName);
+        }
+
+        public void sendEmail()
+        {
+            //SmtpClient client = new SmtpClient();
+            //client.Port = 587;
+            //client.Host = "smtp.gmail.com";
+            //client.EnableSsl = true;
+            //client.Timeout = 10000;
+            //client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //client.UseDefaultCredentials = false;
+            //client.Credentials = new NetworkCredential("dionisiusyose11@gmail.com", "gmaildion1997");
+            //MailMessage mm = new MailMessage("donotreply@gmail.com", user.UserEmail, "Secret!!!!", passwordText);
+            //mm.BodyEncoding = UTF8Encoding.UTF8;
+            //mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+            //client.Send(mm);
         }
     }
 }
