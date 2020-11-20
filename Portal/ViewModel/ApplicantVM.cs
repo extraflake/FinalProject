92e57364fe65552eacd7e72f9957f066d964f2c0
dionisiusyose <dionisiusyose11@gmail.com>
2020-11-20 09:29:12 +0700

    add VM

--- Portal/ViewModel/ApplicantVM.cs
+++ Portal/ViewModel/ApplicantVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,5 +13,12 @@ namespace Portal.ViewModel
         public int FileId { get; set; }
         public int PositionId { get; set; }
         public int ReferenceId { get; set; }
+
+        // Application
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        [MaxLength]
+        public byte[] DataFile { get; set; }
+        public DateTime? CreatedOn { get; set; }
     }
 }
