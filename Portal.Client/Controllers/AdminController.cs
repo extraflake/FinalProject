ecc7e09802881d126c75299e54d5158fe238ca6d
Ridho Novradinata <ridhonovradinata@gmail.com>
2020-11-20 16:11:03 +0700

    add admin panel refrence

--- Portal.Client/Controllers/AdminController.cs
+++ Portal.Client/Controllers/AdminController.cs
@@ -1,13 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Portal.Client.Controllers
 {
     public class AdminController : Controller
     {
+        [HttpGet]
+        public ActionResult Get(Portal.Models.Reference reference)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44307");
+                MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                string data = JsonConvert.SerializeObject(reference);
+                var contentData = new StringContent(data, Encoding.UTF8, "application/json");
+                var response = client.GetAsync("/api/references").Result;
+                //ViewBag.Message = response.Content.ReadAsStringAsync().Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return Json(response.Content.ReadAsStringAsync().Result.ToString());
+                }
+                else
+                {
+                    return Content("GAGAL");
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult Add(Portal.Models.Reference reference)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44307");
+                MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                string data = JsonConvert.SerializeObject(reference);
+                var contentData = new StringContent(data, Encoding.UTF8, "application/json");
+                var response = client.PostAsync("/api/references", contentData).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return Json(new {statusCode = response.StatusCode});
+                }
+                else
+                {
+                    return Content("GAGAL");
+                }
+
+            }
+        }
+
+
         public IActionResult Index()
         {
             return View();
---
+++ Portal.Client/wwwroot/js/ReferenceAdminScript.js
@@ -0,0 +1,82 @@
+﻿var table = null;
+
+$(document).ready(function () {
+    console.log("ready!");
+    $.ajax({
+        "type": "GET",
+        "url": "/Admin/Get",
+        "contentType": "application/json; charset=utf-8",
+        "dataType": "json",
+        "success": function (data) {
+            qt = JSON.parse(data);
+            qt = qt.data
+            console.log(qt);
+            $('#table1').DataTable({
+                data: qt,
+                "oLanguage": {
+                    "sSearch": "Search : "
+                },
+                columns: [
+                    {
+                        render: function (data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    },
+                    {
+                        "data": "id",
+                        "visible": false
+                    },
+                    { "data": "name" },
+                    {
+                        "data": "applicants",
+                        "visible": false
+                    },
+                    {
+                        "render": function (data, type, row) {
+                            return '<button class="btn pull-left hidden-sm-down btn-warning" data-placement="right" data-toggle="tooltip" title = "Edit" onclick="return GetById(' + row.id + ');"><i data-feather="edit">Edit</i></button >' + '&nbsp;' +
+                                '<button class="btn btn-danger" data-placement="right" data-toggle="tooltip" data-animation="false" title="Delete" onclick="return DeleteSchedule(' + row.id + ');"><i data-feather="trash">Delete</i></button>'
+                        }
+                    }]
+            });
+        }
+    });
+});
+function Save() {
+    debugger;
+    var Department = new Object();
+    Department.Name = $('#Name').val();
+    Department.Tanggal = $('#Date').val();
+    if (Department.Name == '') {
+        Swal.fire('Error', 'Please Input Department Name', 'error');
+        ClearScreen();
+    } else {
+        $.ajax({
+            type: 'POST',
+            url: '/Departments/Insert',
+            data: Department
+        }).then((result) => {
+            debugger;
+            if (result.StatusCode == 200) {
+                Swal.fire({
+                    position: 'center',
+                    type: 'success',
+                    icon: 'success',
+                    title: 'Department Added Successfully'
+                });
+                table.ajax.reload();
+            } else {
+                Swal.fire('Error', 'Failed to Input', 'error');
+                ClearScreen();
+                table.ajax.reload();
+            }
+        })
+    }
+}
+
+
+function ClearScreen() {
+    $('#Id').val("");
+    $('#Name').val("");
+    $('#Update').hide();
+    $('#Save').show();
+}
