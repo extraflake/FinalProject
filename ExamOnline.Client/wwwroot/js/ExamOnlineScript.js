a9bfbad955a90e5762f13e0b276f9ede59421c1f
Juandro Sobath Lumbantoruan <69336047+juandelcano@users.noreply.github.com>
2020-11-23 08:42:33 +0700

    Update skoring

--- ExamOnline.Client/wwwroot/js/ExamOnlineScript.js
+++ ExamOnline.Client/wwwroot/js/ExamOnlineScript.js
@@ -1,16 +1,15 @@
 ﻿var num = 1;
 var qt;
+var answer = [];
+var isDoubt = [];
+var totalQuestion = 0;
 
-function Choose() {
-    window.location = "exam/ujian"
+window.console.log = function () {
+    console.error("sepertinya anda mengerti cara ngoding!");
+    window.console.log = function () {
+        return false;
+    }
 }
-
-//window.console.log = function () {
-//    console.error("Sepertinya anda mengerti cara ngoding!");
-//    window.console.log = function () {
-//        return false;
-//    }
-//}
 
 $(document).ready(function () {
     if (num == 1) {
@@ -36,70 +35,77 @@ $(document).ready(function () {
             var listChoices = [qt[num - 1]['answerA'], qt[num - 1]['answerB'], qt[num - 1]['answerC'], qt[num - 1]['answerD']];
             console.log(qt.length);
             listChoices = shuffle(listChoices);
-            //document.getElementById('segmentName').innerHTML = qt[num - 1]['title']
             document.getElementById("question").innerHTML = qt[num - 1]['quest'];
             document.getElementById('segmentName').innerHTML = qt[num - 1]['title'];
             document.getElementById('segmentDuration').innerHTML = "Duration          : " + qt[num - 1]['duration'] + " minutes";
             document.getElementById('questionQuantity').innerHTML = "Question Quantity : " + qt[num - 1]['questionQuantity'];
-            document.getElementById("choiceATxt").innerHTML = listChoices[0];
-            document.getElementById("choiceBTxt").innerHTML = listChoices[1];
-            document.getElementById("choiceCTxt").innerHTML = listChoices[2];
-            document.getElementById("choiceDTxt").innerHTML = listChoices[3];
+            totalQuestion = qt[num - 1]['questionQuantity'];
+            document.getElementById("answerA").innerHTML = listChoices[0];
+            document.getElementById("answerB").innerHTML = listChoices[1];
+            document.getElementById("answerC").innerHTML = listChoices[2];
+            document.getElementById("answerD").innerHTML = listChoices[3];
             var div = document.getElementById('btnNav');
+            intialAnswer();
 
             for (var i = 0; i < qt.length; i++) {
                 div.innerHTML = div.innerHTML +
-                    '<button class="btn btn-light btn-sm mt-1 ml-2" value="' + i + '" onclick="return NavigateToQuestion(' + i + ');">' + (i+1) + '</button>';
+                    '<button class="btn btn-light btn-sm mt-1 ml-2" value="' + i + '" id="'+i+'" onclick="return NavigateToQuestion(' + i + ');">' + (i+1) + '</button>';
             }
-            
         }
     })
 });
 
 function nextQuestion() {
-    console.log(getRadioCheckedValue("choices"));
-    num++;
-    if (num == 1) {
-        $('#prevBtn').hide();
+    if (num == totalQuestion) {
+        answer[num - 1] = getRadioCheckedValue("choices");
+        console.log(answer);
+        finishSegment();
     }
-    else if (num > 1) {
-        $('#prevBtn').show();
-    }
-    console.log(num);
-    document.getElementById('number').innerHTML = num;
-    $(document).ready(function () {
-        console.log("ready!");
-        var listChoices = [qt[num - 1]['answerA'], qt[num - 1]['answerB'], qt[num - 1]['answerC'], qt[num - 1]['answerD']];
-        console.log(listChoices);
-        listChoices = shuffle(listChoices);
-        //document.getElementById('segmentName').innerHTML = qt[num - 1]['title']
-        document.getElementById("question").innerHTML = qt[num - 1]['quest'];
-        document.getElementById("choiceATxt").innerHTML = listChoices[0];
-        document.getElementById("choiceBTxt").innerHTML = listChoices[1];
-        document.getElementById("choiceCTxt").innerHTML = listChoices[2];
-        document.getElementById("choiceDTxt").innerHTML = listChoices[3];
+    else {
+        answer[num - 1] = getRadioCheckedValue("choices");
+        if (isDoubt[num - 1] == true && answer[num - 1] != "") {
+            document.getElementById(num - 1).className = 'btn btn-warning ml-2';
+        }
+        else if (isDoubt[num - 1] == false || answer[num - 1] != "") {
+            document.getElementById(num - 1).className = 'btn btn-primary ml-2';
+        }
+        num++;
+        isFinalQuestion(num);
+        console.log(answer);
+        setRadioCheckedValue("choices");
+        isNotFirstQuestion(num);
 
-        
-    });
+        $(document).ready(function () {
+            document.getElementById('number').innerHTML = num;
+            var listChoices = [qt[num - 1]['answerA'], qt[num - 1]['answerB'], qt[num - 1]['answerC'], qt[num - 1]['answerD']];
+            listChoices = shuffle(listChoices);
+            document.getElementById("question").innerHTML = qt[num - 1]['quest'];
+            document.getElementById("answerA").innerHTML = listChoices[0];
+            document.getElementById("answerB").innerHTML = listChoices[1];
+            document.getElementById("answerC").innerHTML = listChoices[2];
+            document.getElementById("answerD").innerHTML = listChoices[3];
+        });
+    }
 }
 
 function previousQuestion() {
     num--;
-    if (num == 1) {
-        $('#prevBtn').hide();
-    }
-    else if (num > 1) {
-        $('#prevBtn').show();
-    }
+    isNotFirstQuestion(num);
+    isFinalQuestion(num);
     document.getElementById('number').innerHTML = num;
     $(document).ready(function () {
-        console.log("ready!");
         var listChoices = [qt[num - 1]['answerA'], qt[num - 1]['answerB'], qt[num - 1]['answerC'], qt[num - 1]['answerD']];
         document.getElementById("question").innerHTML = qt[num - 1]['quest'];
-        document.getElementById("choiceATxt").innerHTML = listChoices[0];
-        document.getElementById("choiceBTxt").innerHTML = listChoices[1];
-        document.getElementById("choiceCTxt").innerHTML = listChoices[2];
-        document.getElementById("choiceDTxt").innerHTML = listChoices[3];
+        document.getElementById("answerA").innerHTML = listChoices[0];
+        document.getElementById("answerB").innerHTML = listChoices[1];
+        document.getElementById("answerC").innerHTML = listChoices[2];
+        document.getElementById("answerD").innerHTML = listChoices[3];
+        var radios = document.getElementsByName("choices");
+        for (var i = 0; i < listChoices.length; i++) {
+            if (listChoices[i] == answer[num-1]) {
+                radios[i].checked = true;
+            }  
+        }
     });
 }
 
@@ -120,27 +126,98 @@ function getRadioCheckedValue(radio_name) {
     var radios = document.getElementsByName(radio_name);
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
-            alert(radios[i].value);
-            break;
+            var radVal = radios[i].value;
+            return document.getElementById(radVal).innerHTML;
+        }
+    }
+
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked == false) {
+            return "";
         }
     }
 }
 
 function NavigateToQuestion(number) {
-    if (number == 0) {
-        $('#prevBtn').hide();
-    }
-    else if (number > 0) {
-        $('#prevBtn').show();
-    }
+    num = number + 1;
+    isNotFirstQuestion((number + 1));
+    isFinalQuestion(number+1);
     document.getElementById('number').innerHTML = number+1;
     $(document).ready(function () {
-        console.log("ready!");
         var listChoices = [qt[number]['answerA'], qt[number]['answerB'], qt[number]['answerC'], qt[number]['answerD']];
         document.getElementById("question").innerHTML = qt[number]['quest'];
-        document.getElementById("choiceATxt").innerHTML = listChoices[0];
-        document.getElementById("choiceBTxt").innerHTML = listChoices[1];
-        document.getElementById("choiceCTxt").innerHTML = listChoices[2];
-        document.getElementById("choiceDTxt").innerHTML = listChoices[3];
+        document.getElementById("answerA").innerHTML = listChoices[0];
+        document.getElementById("answerB").innerHTML = listChoices[1];
+        document.getElementById("answerC").innerHTML = listChoices[2];
+        document.getElementById("answerD").innerHTML = listChoices[3];
+        var radios = document.getElementsByName("choices");
+        for (var i = 0; i < listChoices.length; i++) {
+            if (listChoices[i] == answer[number]) {
+                radios[i].checked = true;
+            }
+        }
     });
+}
+
+function setRadioCheckedValue(radio_name) {
+    var radios = document.getElementsByName(radio_name);
+    for (var i = 0, length = radios.length; i < length; i++) {
+        radios[i].checked = false;
+    }
+}
+
+function doubtFull() {
+    document.getElementById(num - 1).className = 'btn btn-warning ml-2';
+    isDoubt[num - 1] = true;
+    nextQuestion();
+}
+
+function isFinalQuestion(number) {
+    if (number < totalQuestion) {
+        document.getElementById('nextBtn').innerHTML = 'Next<i class="fa fa-arrow-right"></i>';
+        return false;
+    }
+    else if (number == totalQuestion) {
+        document.getElementById('nextBtn').innerHTML = 'Submit my Answer<i class="fa fa-arrow-right"></i>';
+        return true;
+    }
+    return "";
+}
+
+function isNotFirstQuestion(number) {
+    if (number == 1) {
+        $('#prevBtn').hide();
+        return false;
+    }
+    else if (number > 1) {
+        $('#prevBtn').show();
+        return true;
+    }
+    return "";
+}
+
+function intialAnswer() {
+    for (var i = 0; i < totalQuestion; i++) {
+        answer[i] = "";
+        isDoubt[i] = false;
+    }
+    return "";
+}
+
+function finishSegment() {
+    var score = 0;
+    for (var i = 0; i < answer.length; i++) {
+        console.log(answer[i]);
+        console.log(qt[i]['correctAnswer']);
+        if (answer[i] == qt[i]['correctAnswer']) {
+
+            score += qt[i]['point'];
+        }
+    }
+    Swal.fire({
+        position: 'center',
+        type: 'info',
+        icon: 'info',
+        title: 'Your score : ' + score
+    });;
 }
