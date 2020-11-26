4e7b105d1ff9fe08926bc572f04918be3361f956
agung ali akbar <agungaliakbar8@gmail.com>
2020-11-26 09:30:45 +0700

    addclient

--- UserManagement/Startup.cs
+++ UserManagement/Startup.cs
@@ -47,6 +47,7 @@ namespace UserManagement
                 };
             });
 
+            services.AddScoped<UserApplicationRepository>();
             services.AddScoped<EmployeeRepository>();
             services.AddScoped<UniversityRepository>();
             services.AddScoped<DepartmentRepository>();
